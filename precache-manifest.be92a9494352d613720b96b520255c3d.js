6b6eb8d3c066a92ec5d0842458f8394e3b4d2c82
Cody Johnson <codymj16@aol.com>
2021-03-07 23:40:41 -0700

    Updates

---
+++ precache-manifest.be92a9494352d613720b96b520255c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c6195d3bc55f3f6f9123e5ab20198b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f14bcc009643eaa075f5",
+    "url": "/react-portfolio/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "9d1871fa39f549ad368b",
+    "url": "/react-portfolio/static/css/main.f74b0431.chunk.css"
+  },
+  {
+    "revision": "f14bcc009643eaa075f5",
+    "url": "/react-portfolio/static/js/2.e70e1fc3.chunk.js"
+  },
+  {
+    "revision": "9d1871fa39f549ad368b",
+    "url": "/react-portfolio/static/js/main.35288571.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  }
+]);
